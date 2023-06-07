7ba9d6d3709b6db6f452176978f0b58ec49d9134
atakanozturk98 <ozturkatakan000@gmail.com>
2023-06-07 04:33:26 +0300

    React_06.06.2023

--- React_06.06.2023/football_site/src/components/EditDetail.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class EditDetail extends Component {
-  render() {
-    return (
-      <div>EditDetail</div>
-    )
-  }
-}
--- React_06.06.2023/football_site/src/components/TotalMarketValue.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class TotalMarketValue extends Component {
-  render() {
-    return (
-      <div>totalMarketValue</div>
-    )
-  }
-}
